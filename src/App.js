ec4b1029be7d9f4952c62cdef1f0f5f1d609aaae
Jay2502Gajjar <70512023+Jay2502Gajjar@users.noreply.github.com>
2026-02-03 22:18:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from "./components/navbar";
+import Hero from "./components/h";
+import Player from "./components/player"
+import BackgroundVideo from "./components/BackgroundVideo";
+
+function App() {
+  return (
+    <div>
+      <BackgroundVideo />
+      <Navbar />
+      <Hero />
+      <Player />
+      
+    </div>
+  );
+}
+
+export default App;

d1409e1b3706b81910ecc4a7249812237db5de00
Jay2502Gajjar <70512023+Jay2502Gajjar@users.noreply.github.com>
2026-02-03 22:18:59 +0530

    Add files via upload

---
+++ components/navbar.js
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <div style={{
+      padding: "20px 40px",
+      fontSize: "20px",
+      fontWeight: "bold",
+      borderBottom: "1px solid #1e293b"
+    }}>
+      Welcome
+    </div>
+  );
+}
+
+export default Navbar;
